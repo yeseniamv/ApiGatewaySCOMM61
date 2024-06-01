c43f2cf39fa8098ab6ae85a7d77d82c8cf12a662
Juan Martinez <jfmg.64@gmail.com>
2024-06-01 13:11:46 -0700

    Agregando Entidad Usuario

--- ApiGateway.SqlServer/ApplicationDbContext.cs
+++ ApiGateway.SqlServer/ApplicationDbContext.cs
@@ -1,4 +1,12 @@
-﻿internal class ApplicationDbContext
-{
+﻿using ApiGateway.SqlServer.Entities;
+using Microsoft.EntityFrameworkCore;
 
+internal class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) :  base(options)
+    {
+
+    }
+
+    public DbSet<Usuario> Usuarios { get; set; }
 }
---
+++ ApiGateway.SqlServer/Entities/Usuario.cs
@@ -0,0 +1,10 @@
+﻿namespace ApiGateway.SqlServer.Entities
+{
+    public class Usuario
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
