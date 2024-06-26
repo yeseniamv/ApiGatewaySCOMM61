25ec3fdcd3eb3571236ae2cd54e85aeb54d78670
yeseniamv <98119293+yeseniamv@users.noreply.github.com>
2024-06-26 09:41:59 -0700

    Add files via upload

---
+++ ApiGateway.SqlServer/Entities/Venta.cs
@@ -0,0 +1,11 @@
+﻿namespace ApiGateway.SqlServer.Entities
+{
+    public class Venta
+    {
+        public Guid Id { get; set; }
+        public Guid UsuarioId { get; set; }
+        public Usuario? Usuario { get; set; }
+        public string JuegoId { get; set; }
+        public DateTime Fecha { get; set; }
+    }
+}
